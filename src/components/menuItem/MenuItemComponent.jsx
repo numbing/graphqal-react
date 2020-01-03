6337acb30be006d18a6bd949377da31713f1ad7f
mir <mirnumbing@gmail.com>
2020-01-03 17:20:55 +0100

    add some design here and there

--- src/components/menuItem/MenuItemComponent.jsx
+++ src/components/menuItem/MenuItemComponent.jsx
@@ -3,15 +3,18 @@ import "./menuItemStyle.scss";
 
 const MenuItemComponent = ({ title, imageUrl, size }) => {
   return (
+    <div className={`${size} menu-item`}>
     <div
-      style={{ backgroundImage: `url(${imageUrl})` }}
-      className={`${size} menu-item`}
-    >
-      <div className="content">
-        <h1 className="title">{title}</h1>
-        <span className="subtitle"></span>
-      </div>
+      className='background-image'
+      style={{
+        backgroundImage: `url(${imageUrl})`
+      }}
+    />
+    <div className='content'>
+      <h1 className='title'>{title.toUpperCase()}</h1>
+      <span className='subtitle'>SHOP NOW</span>
     </div>
+  </div>
   );
 };
 
