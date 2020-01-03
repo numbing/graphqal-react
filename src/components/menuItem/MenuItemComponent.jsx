a03d1194d9e01d704d50214a308baea49ce74194
mir <mirnumbing@gmail.com>
2020-01-03 17:10:09 +0100

    add and design some static data later will make it dynamic

--- src/components/menuItem/MenuItemComponent.jsx
+++ src/components/menuItem/MenuItemComponent.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import "./menuItemStyle.scss";
 
-const MenuItemComponent = ({ title, imageUrl }) => {
+const MenuItemComponent = ({ title, imageUrl, size }) => {
   return (
-    <div style={{ backgroundImage: `url(${imageUrl})` }} className="menu-item">
+    <div
+      style={{ backgroundImage: `url(${imageUrl})` }}
+      className={`${size} menu-item`}
+    >
       <div className="content">
         <h1 className="title">{title}</h1>
         <span className="subtitle"></span>
