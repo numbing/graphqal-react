8ea53c174c263f66cc07141b19ae81805fac0744
mir <mirnumbing@gmail.com>
2020-01-06 13:08:03 +0100

    style for shop page

--- src/components/previewCollection/CollectionPreview.jsx
+++ src/components/previewCollection/CollectionPreview.jsx
@@ -1,15 +1,18 @@
 import React from "react";
+import CollectionItem from "../collectionItem/CollectionItem";
+import './collectionPreview.scss'
 
 const CollectionPreview = ({ title, items }) => {
   return (
     <div className="collection-preview">
       <h1 className="title">{title.toUpperCase()}</h1>
-      <div className="preview"></div>
-      {items
-        .filter((item, idx) => idx < 4)
-        .map(item => {
-          return <div key={item.id}>{item.name}</div>;
-        })}
+      <div className="preview">
+        {items
+          .filter((item, idx) => idx < 4)
+          .map(({ id, ...otherItemProps }) => (
+            <CollectionItem key={id} {...otherItemProps} />
+          ))}
+      </div>
     </div>
   );
 };
