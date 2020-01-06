af2d311eb564d53edebfff8c48b8fcee40c5e8e5
mir <mirnumbing@gmail.com>
2020-01-06 12:08:16 +0100

    add router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import "./App.css";
 import HomePageComponent from "./pages/homePage/HomePgaeComponent";
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="test">
-      <HomePageComponent />
+      <Switch>
+        <Route exact path="/" component={HomePageComponent} />
+      </Switch>
     </div>
   );
 }
--- src/components/menuItem/MenuItemComponent.jsx
+++ src/components/menuItem/MenuItemComponent.jsx
@@ -1,21 +1,32 @@
 import React from "react";
 import "./menuItemStyle.scss";
+import { withRouter } from "react-router-dom";
 
-const MenuItemComponent = ({ title, imageUrl, size }) => {
+const MenuItemComponent = ({
+  title,
+  imageUrl,
+  size,
+  history,
+  linkUrl,
+  match
+}) => {
   return (
-    <div className={`${size} menu-item`}>
     <div
-      className='background-image'
-      style={{
-        backgroundImage: `url(${imageUrl})`
-      }}
-    />
-    <div className='content'>
-      <h1 className='title'>{title.toUpperCase()}</h1>
-      <span className='subtitle'>SHOP NOW</span>
+      className={`${size} menu-item`}
+      onClick={() => history.push(`${match.url}${linkUrl}`)}
+    >
+      <div
+        className="background-image"
+        style={{
+          backgroundImage: `url(${imageUrl})`
+        }}
+      />
+      <div className="content">
+        <h1 className="title">{title.toUpperCase()}</h1>
+        <span className="subtitle">SHOP NOW</span>
+      </div>
     </div>
-  </div>
   );
 };
 
-export default MenuItemComponent;
+export default withRouter(MenuItemComponent);
