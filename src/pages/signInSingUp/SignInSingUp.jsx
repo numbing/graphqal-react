7939e70bddfee53b90e6626a493e3a6ac1f0e119
mir <mirnumbing@gmail.com>
2020-01-08 16:31:29 +0100

    implement signup

--- src/pages/signInSingUp/SignInSingUp.jsx
+++ src/pages/signInSingUp/SignInSingUp.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import SignIn from "../../components/signIn/SignIn";
+import SignUp from "../../components/signUp/SignUp";
+
+import "./signInSingUp.scss"
 
 const SignInSingUp = () => {
   return (
     <div className="sign-in-and-sign-up">
       <SignIn />
+      <SignUp/>
     </div>
   );
 };
